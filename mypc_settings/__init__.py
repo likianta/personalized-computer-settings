ddb0d08707f0dcc15b2f1345ba31404918f16ffa
likianta <likianta@foxmail.com>
2024-04-14 09:33:36 +0800

    make win32 shortcut

---
+++ mypc_settings/__init__.py
@@ -0,0 +1,1 @@
+from .make_shortcut import make_shortcut_win32
--- mypc_settings/make_shortcut.py
+++ mypc_settings/make_shortcut.py
@@ -1,10 +1,13 @@
 import os
 import re
 import sys
+from textwrap import dedent
 
 from lk_utils import call_once
+from lk_utils import dumps
 from lk_utils import fs
 from lk_utils import mklink
+from lk_utils import run_cmd_args
 from lk_utils import timestamp
 
 
@@ -29,12 +32,18 @@ def main() -> None:
         #     }
         case 'win32':
             io_map = {
+                'applist'                               : '...',
                 'apps'                                  : '...',
-                'apps/nushell/nu.exe'                   : 'shortcut/nushell.exe',
+                # 'apps/nushell/nu.exe'                   : 'shortcut/nushell.exe',
                 'apps/python/3.12'                      : 'shortcut/python-3.12',
+                'backups'                               : '...',
+                'backups/software-packages'             : '...',
+                'backups/software-settings'             : '...',
                 'documents'                             : '...',
                 'documents/appdata'                     : '...',
                 'documents/gitbook-ssg'                 : 'shortcut/gitbook',
+                'downloads'                             : '...',
+                'pictures'                              : '...',
                 'temp'                                  : '...',
                 'temp/{date}'                           : '...',
                 'workspace'                             : '...',
@@ -68,7 +77,47 @@ def main() -> None:
             o = f'shortcut/temp-({yyyy_mm})'
         
         print('[red]{}[/] -> [green]{}[/]'.format(i, o), ':ir')
-        mklink(i, o)
+        if sys.platform == 'win32':
+            make_shortcut_win32(i, o + '.lnk')
+        else:
+            mklink(i, o)
+
+
+def make_shortcut_win32(file_i: str, file_o: str = None) -> None:
+    """
+    use batch script to create shortcut, no pywin32 required.
+
+    args:
+        file_o: if not given, will create a shortcut in the same folder as -
+            `file_i`, with the same base name.
+
+    https://superuser.com/questions/455364/how-to-create-a-shortcut-using-a
+    -batch-script
+    https://www.blog.pythonlibrary.org/2010/01/23/using-python-to-create
+    -shortcuts/
+    """
+    assert os.path.exists(file_i) and not file_i.endswith('.lnk')
+    if not file_o:
+        file_o = fs.replace_ext(file_i, 'lnk')
+    else:
+        assert file_o.endswith('.lnk')
+    if os.path.exists(file_o):
+        os.remove(file_o)
+    
+    vbs = '_temp.vbs'
+    command = dedent('''
+        Set objWS = WScript.CreateObject("WScript.Shell")
+        lnkFile = "{file_o}"
+        Set objLink = objWS.CreateShortcut(lnkFile)
+        objLink.TargetPath = "{file_i}"
+        objLink.Save
+    ''').format(
+        file_i=fs.abspath(file_i).replace('/', '\\'),
+        file_o=fs.abspath(file_o).replace('/', '\\'),
+    )
+    dumps(command, vbs, ftype='plain')
+    
+    run_cmd_args('cscript', '/nologo', vbs)
 
 
 def _find_latest_date(dir: str) -> str:
