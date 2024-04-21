05abd7a0527b5623ee13f52cbb1973ec460a8ae4
likianta <likianta@foxmail.com>
2024-04-21 11:57:03 +0800

    improve path resolve

--- mypc_settings/make_shortcut.py
+++ mypc_settings/make_shortcut.py
@@ -27,28 +27,21 @@ def main() -> None:
     
     # noinspection PyCompatibility
     match sys.platform:
-        # case 'linux':
-        #     io_map = {
-        #         'temp/{date}': '...',
-        #     }
         case 'win32':
             io_map = loads(fs.xpath('../config/windows_shortcuts.yaml'))['map']
         case _:
             raise NotImplementedError
     
     for i, o in io_map.items():
-        if '{' in i:
-            a, b = i.rsplit('/', 1)
-            i = a + '/' + _fill_name(b, a)
+        i = finish_path(i)
         if not fs.exists(i):
             print(':iv3', f'could not find "{i}"')
             continue
-            
+        
         if o == '...':
             o = 'shortcut/{}'.format(i.rsplit('/', 1)[-1])
-        elif '{' in o:
-            a, b = o.rsplit('/', 1)
-            o = a + '/' + _fill_name(b)
+        else:
+            o = finish_path(o)
         assert o.startswith('shortcut/')
         
         print('[red]{}[/] -> [green]{}[/]'.format(i, o), ':ir')
@@ -97,19 +90,25 @@ def make_shortcut_win32(file_i: str, file_o: str = None) -> None:
     run_cmd_args('cscript', '/nologo', vbs)
 
 
-def _fill_name(name: str, dir: str = None) -> str:
-    # static
-    name = (
-        name
-        .replace('{yyyy}', timestamp('y'))
-        .replace('{mm}', timestamp('m'))
-    )
-    # dynamic
-    if '{date}' in name:
-        name = name.replace('{ver}', _find_latest_date(dir))
-    if '{ver}' in name:
-        name = name.replace('{ver}', _find_latest_version(dir))
-    return name
+def finish_path(path: str) -> str:
+    _parent = fs.parent(path)  # a dir path
+    
+    def inplace(m: re.Match) -> str:
+        item = m.group(1)
+        match item:
+            case 'date':
+                return _find_latest_date(_parent)
+            case 'mm':
+                return timestamp('m')
+            case 'user_home':
+                assert sys.platform == 'win32'
+                return 'C:/Users/Likianta'  # TODO
+            case 'yyyy':
+                return timestamp('y')
+            case 'ver':
+                return _find_latest_version(_parent)
+    
+    return re.sub(r'<(\w+)>', inplace, path)
 
 
 def _find_latest_date(dir: str) -> str:
