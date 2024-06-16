06bfe22ac9359690b34566e70b6ed1dff921fd12
Likianta <likianta@foxmail.com>
2024-06-17 00:50:27 +0800

    make dev-dist folder

---
+++ mypc_settings/dev_dist.py
@@ -0,0 +1,27 @@
+from argsense import cli
+from lk_utils import fs
+
+from mypc_settings import common
+
+
+@cli.cmd()
+def main(
+    config_file: str = fs.xpath('../config/dev_dist/map.yaml'),
+) -> None:
+    cfg = common.loads_config(config_file)
+    root = common.reformat_path(cfg['workspace_root'])
+    print(root)
+    for k, v in cfg['symlinks'].items():
+        dir_i = common.reformat_path(k)
+        dir_o = '{}/dev-dist/{}'.format(root, v)
+        assert dir_i.endswith('/dist')
+        common.print_conversion(fs.relpath(dir_i, root), f'dev_dist/{v}')
+        if fs.exists(dir_i):
+            fs.make_link(dir_i, dir_o)
+        else:
+            print('^   skip non-existent "dist" folder', ':vs')
+
+
+if __name__ == '__main__':
+    # pox mypc_settings/dev_dist.py
+    cli.run(main)
